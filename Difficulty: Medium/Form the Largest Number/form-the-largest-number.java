f1bb9935456591ca63be36be0c168d5150a65217
Dilip R <128499989+DilipRavikumar@users.noreply.github.com>
2025-01-16 18:30:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        while (t-- > 0) {
+            String input = sc.nextLine();
+            String[] numbers = input.split(" ");
+            int[] arr = new int[numbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                arr[i] = Integer.parseInt(numbers[i]);
+            }
+
+            Solution ob = new Solution();
+            String ans = ob.findLargest(arr);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    String findLargest(int[] arr) {
+        String[] strArr = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            strArr[i] = String.valueOf(arr[i]);
+        }
+
+        Arrays.sort(strArr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strArr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder largestNum = new StringBuilder();
+        for (String s : strArr) {
+            largestNum.append(s);
+        }
+
+        return largestNum.toString();
+    }
+}
